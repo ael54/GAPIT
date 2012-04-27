16e8daa509acc2be724e94c8d39bd22a9e497b8c
Zhiwu Zhang <zz19@cornell.edu>
2012-04-27 10:22:20 -0400

    .

---
+++ GAPIT.0000.R
@@ -0,0 +1,11 @@
+################################################################################
+#GAPIT – Genome Association and Prediction Integrated Tool
+#This is an R package that performs Genome Wide Association Study (GWAS) and 
+# genome prediction (or selection). #This program uses state-of-the-art methods 
+#developed for statistical genetics, such as the unified mixed model, EMMA, 
+#the compressed mixed linear model, and P3D/EMMAx.
+
+#Designed by Zhiwu Zhang
+#Writen by Alex Lipka, Feng Tian and Zhiwu Zhang
+################################################################################
+
