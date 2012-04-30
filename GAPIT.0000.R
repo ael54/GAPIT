6b118554b05ebb5af2f9bfa115c3d3eddefbbd0e
Zhiwu Zhang <zz19@cornell.edu>
2012-04-30 09:56:08 -0400

    Modification of GAPIT.0000.R file (intro)

--- GAPIT.0000.R
+++ GAPIT.0000.R
@@ -1,5 +1,5 @@
 ################################################################################
-#GAPIT – Genome Association and Prediction Integrated Tool
+#GAPIT: Genome Association and Prediction Integrated Tool
 #This is an R package that performs Genome Wide Association Study (GWAS) and 
 # genome prediction (or selection). #This program uses state-of-the-art methods 
 #developed for statistical genetics, such as the unified mixed model, EMMA, 
